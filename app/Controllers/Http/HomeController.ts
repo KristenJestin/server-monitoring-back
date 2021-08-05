6e4c8a81d8c557bba659e11f31a9c9a4b8bd95a7
Kristen Jestin <kris.jestin@laposte.net>
2021-08-05 18:33:05 +0200

    feat: add homepage data

--- app/Controllers/Http/HomeController.ts
+++ app/Controllers/Http/HomeController.ts
@@ -1,7 +1,15 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
+import Drive from 'App/Models/Drive'
+import { getDrivesInfo } from 'App/Services/Filesystem'
+import Application from 'App/Models/Application'
+
 export default class HomeController {
     public async index({ inertia }: HttpContextContract) {
-        return inertia.render('Home')
+        const drivesData = await Drive.all()
+        const drives = await getDrivesInfo(drivesData)
+        const applications = await Application.all()
+
+        return inertia.render('Home', { drives, applications })
     }
 }
